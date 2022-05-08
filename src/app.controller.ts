ccd939f34e04eebfb725cd781663d4200df35dea
hwang <wwasgktps23@gmail.com>
2022-05-08 20:27:22 +0900

    initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/")
+  getI() {
+    return "asd"
+  }
+
+  @Get("/comments")
+  getHello(@Req() test: Request): string {
+    const fs = require('fs');
+    const text = 'test';
+    fs.writeFileSync("target.txt", '\ufeff' + text, {encoding: 'utf8'});
+    return test.query.test + "\t" +text;
+  }
+
+  @Post("/post")
+  postVal(@Body() body): string {
+    const fs = require('fs');
+    const text = body.Value
+    fs.writeFileSync("post.txt", '\ufeff' + text, {encoding: 'utf8'});
+    return ;
   }
 }
+
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(h:Number, t:Number, wt:Number, ecc:Number): string {
+    let input:any = {h:Number, t:Number, wt:Number, ecc:Number};
+    input.h = h; input.t = t; input.wt = wt; input.ecc = ecc;
+    
+    function printShit(){
+      let shit:string = "temp = " + input.t + " humidity = " + input.h + " water temp = " + input.wt + " ecc = " + input.ecc + "\n"
+      return shit;
+    };
+  var shit:string = printShit();
+  return shit;
   }
 }
